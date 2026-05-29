69a266dcc41c27273305edd5b4db6564ded373c1
CfelipeP <carlos.felipe.perez.escobar.25@gmail.com>
2026-05-29 12:54:24 -0600

    Se agrego la imagen .svg del logo para la pagina, se soluciono el error de local, como? nose... pero se arreglo

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,9 @@ export default defineConfig({
     port: 5173,
     allowedHosts: true,
     proxy: {
-      "/api": {
-        target: "http://localhost:5173",
+      "/backend": {
+        target: "http://Numeros-y-Futbol.test",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
